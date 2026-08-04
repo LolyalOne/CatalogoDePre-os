e2597b2ac6055fb0f4cb94eeec4fbf75130934d4
LolyalOne <mikaellben01@gmail.com>
2026-08-04 16:44:18 -0300

    Adicionar iframes como previews e melhorar o catálogo

--- EspetoMenu/src/lib/firebase.ts
+++ EspetoMenu/src/lib/firebase.ts
@@ -4,10 +4,10 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA-q8LKOGlbbRo9blAiOqN69C10ou7swTU',
-  authDomain: 'catalogo-demo.firebaseapp.com',
-  databaseURL: 'https://catalogo-demo-default-rtdb.firebaseio.com',
-  projectId: 'catalogo-demo',
-  storageBucket: 'catalogo-demo.firebasestorage.app',
+  authDomain: 'espeto-f-cil-menu.firebaseapp.com',
+  databaseURL: 'https://espeto-f-cil-menu-default-rtdb.firebaseio.com',
+  projectId: 'espeto-f-cil-menu',
+  storageBucket: 'espeto-f-cil-menu.firebasestorage.app',
   messagingSenderId: '549217212218',
   appId: '1:549217212218:web:81756bb071311cfdd92203',
   measurementId: 'G-91Z5GLT7QE'
